a3e435100f8f183e373f8dd84466c57aada17507
noah1216 <76505928+noah1216@users.noreply.github.com>
2021-02-25 12:40:06 +0900

    Update address_seller_spec.rb

--- spec/models/address_seller_spec.rb
+++ spec/models/address_seller_spec.rb
@@ -1,13 +1,27 @@
-
-
 require 'rails_helper'
 
 RSpec.describe AddressSeller, type: :model do
-  describe 'ユーザー新規登録' do
-    it 'nicknameが空では登録できない' do
-      address_seller = AddressSeller.new(postal_code: '', area: 'ここ', city: 'ここ', block_number: 'ここ', phone_number: '11111111111')
-        address_seller.valid?
-        expect(address_seller.errors.full_messages).to include("Postal code can't be blank", "Postal code is invalid")
+  describe '#create' do
+    before do
+      @address_seller = FactoryBot.build(:address_seller)
+    end
+
+    context '商品購入できるとき' do
+      it '全ての項目の入力が存在すれば登録できること' do
+        expect(@address_seller).to be_valid
+      end
+      it '建物名が存在しなくても登録できること' do
+        @address_seller.house_number = ""
+        expect(@address_seller).to be_valid
+      end
+    end
+
+    context '商品購入できないとき' do
+      it '郵便番号が空では登録できない' do
+        @address_seller.postal_code = ''
+        @address_seller.valid?
+        expect(@address_seller.errors.full_messages).to include()
+      end
     end
   end
 end
